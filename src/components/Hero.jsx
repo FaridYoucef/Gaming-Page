88bc81f9389acbb48ea888b7b5e83a976a9f2581
FaridYoucef <songortekinn@gmail.com>
2024-07-20 17:57:29 +0100

    -Setting the Hero background image - Adding the text on the Hero -make the navBar fixed

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./styles/hero.css";
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className='hero-content'>
+            <h1>Play & Compete in your Favorite Game with Player WorldWide </h1>
+            <p>Join your Fellow Players and have Fun</p>
+            <button>Play Now</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Hero
