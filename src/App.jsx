61a98f8b2ea616502ad9bf06a98ce8f3b59c1740
FaridYoucef <songortekinn@gmail.com>
2024-07-23 08:53:21 +0100

    -Create the Tournaments file -Adding the image and setting the text

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Games from './components/Games';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Tournaments from './components/Tournaments';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     < Header />
     < Hero />
     < Games />
+    < Tournaments />
     </>
   )
 }
---
+++ src/components/Tournaments.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./styles/tournaments.css"
+const Tournaments = () => {
+  return (
+    <div className='container'>
+        <img src="/assets/images/ghost.png" alt="character" />
+        <div className='tournements-text'>
+            <h1>Challenging Tournaments</h1>
+            <p>Join Tournaments to Compete with skillful players.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Tournaments
