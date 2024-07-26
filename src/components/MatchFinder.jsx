0c0b80e533fe381c9e086b5cb70664248532b45e
FaridYoucef <songortekinn@gmail.com>
2024-07-26 05:06:53 +0100

    -Replace the li by Buttons -style  the title

--- src/components/MatchFinder.jsx
+++ src/components/MatchFinder.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import './styles/matchFinder.css';
-
+import { TiArrowSortedDown } from "react-icons/ti";
 const MatchFinder = () => {
   return (
-  <section>
+  <section className='match-container'>
     <div className="match-tite">
       <h1>FIND A GAME</h1>
       <p>Pick a the game, the rule and the price</p>
     </div>
 
     <div className="match-menu">
-      <ul>
-        <li>Games</li>
-        <li>Platforms</li>
-      </ul>
+      <button>
+        Games
+        <TiArrowSortedDown />
+      </button>
+      <button>
+        Platforms
+        <TiArrowSortedDown />
+      </button>
     </div>
   </section>
   )
