9a7a7bed75ce1e67afd8ea3124cfaabee2ea0263
FaridYoucef <songortekinn@gmail.com>
2024-08-03 21:01:31 +0100

    -Creating PlatyerPrise.css -s#Styling the player container -Styling the title

--- src/components/PlayersPrise.jsx
+++ src/components/PlayersPrise.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import "./styles/playerPrise.css";
 
 function PlayersPrise() {
   return (
     <div className="player-container">
-      <h1>PLAYER OF THE WEEK</h1>
-      <p>Test your skills and see your name every Sunday at 11AM</p>
+      <div className="player-title">
+        <h1>PLAYER OF THE WEEK</h1>
+        <p>Test your skills and see your name every Sunday at 11AM</p>
+      </div>
 
       <div className="payerCard-container">
         <div className="palyer-card">
