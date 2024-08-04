de23555a78d041aa50ff97afa571f01d30986421
FaridYoucef <songortekinn@gmail.com>
2024-08-04 08:29:04 +0100

    -Styling the text -Styling the Button -Adding a border on the top of the player card

--- src/components/PlayersPrise.jsx
+++ src/components/PlayersPrise.jsx
@@ -3,18 +3,18 @@ import "./styles/playerPrise.css";
 
 function PlayersPrise() {
   return (
-    <div className="player-container">
+    <div className="player-container">    
       <div className="player-title">
         <h1>PLAYER OF THE WEEK</h1>
         <p>Test your skills and see your name every Sunday at 11AM</p>
       </div>
 
-      <div className="payerCard-container">
+      <div className="palyerCard-container">
         <div className="palyer-card">
           <img src="/assets/images/player.jpg" alt="player" />
           <h1>Elgorn</h1>
-          <p>1354 XP ganied 08/26-09/02</p>
-          <p>+20 Xcoins</p>
+          <p>1354 XP gained 08/26-09/02</p>
+          <p className="coins">+20 Xcoins</p>
           <button>View Profile</button>
         </div>
       </div>
