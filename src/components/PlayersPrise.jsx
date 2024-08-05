9dfe9c4a4a3c66c7e11614e40260bf90d975e723
FaridYoucef <songortekinn@gmail.com>
2024-08-05 07:17:31 +0100

    -Updating the palayers' name -Updating the palyers' photo

--- src/components/PlayersPrise.jsx
+++ src/components/PlayersPrise.jsx
@@ -18,15 +18,15 @@ function PlayersPrise() {
           <button>View Profile</button>
         </div>
         <div className="palyer-card card-1">
-          <img src="/assets/images/player.jpg" alt="player" />
-          <h1>Elgorn</h1>
+          <img src="/assets/images/player1.jpg" alt="player" />
+          <h1>MasterofDesaster</h1>
           <p>1354 XP gained 08/26-09/02</p>
           <p className="coins">+20 Xcoins</p>
           <button>View Profile</button>
         </div>
         <div className="palyer-card">
-          <img src="/assets/images/player.jpg" alt="player" />
-          <h1>Elgorn</h1>
+          <img src="/assets/images/player2.webp" alt="player" />
+          <h1>Setou1</h1>
           <p>1354 XP gained 08/26-09/02</p>
           <p className="coins">+20 Xcoins</p>
           <button>View Profile</button>
