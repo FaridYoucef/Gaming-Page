ca766f3dc55905ffa3c13ea88161a00d1b88ea58
FaridYoucef <songortekinn@gmail.com>
2024-08-08 04:52:35 +0100

    -Add a button and style it

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -13,23 +13,25 @@ const News = () => {
         <div className="news1">
           <img src="/public/assets/images/codGpass.jpeg" alt="" />
           <div className="new1-content">
-            <h3>COD to go to Game Pass in gaming shake-up</h3>
+            <h4>COD to go to Game Pass in gaming shake-up</h4>
             <h5>August 24</h5>
             <p>
               Microsoft has torn up the rules of big video game launches by
               announcing its most eagerly awaited new title...
             </p>
+            <button>Learn More</button>
           </div>
         </div>
         <div className="news1">
           <img src="/public/assets/images/fortnitebpass.jpg" alt="" />
           <div className="new1-content">
-            <h3>Battle Pass item causing a huge backlash among the fans</h3>
+            <h4>Battle Pass causing a backlash among the fans</h4>
             <h5>July 24</h5>
             <p>
               Nobody likes their stuff being removed due to an error, even when
-              recompensed. Of course that stings, and Fortnite players...
+              recompensed. Of course that stings...
             </p>
+            <button>Learn More</button>
           </div>
         </div>
       </div>
