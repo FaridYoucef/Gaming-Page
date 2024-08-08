1d86bc8f08194dbb22cdb3f36ab4549237017afc
FaridYoucef <songortekinn@gmail.com>
2024-08-08 04:33:56 +0100

    -Ajusting the tags and the text -Style the news card with an image as backgroud -Add a light dark background with z-index 0 and add the news content on it -Adding the imges

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,7 +3,7 @@ import "./styles/news.css";
 
 const News = () => {
   return (
-    <div className="news-container">
+    <section className="news-container">
       <div className="news-title">
         <h2>LATEST</h2>
         <h1 className="h1">NEWS</h1>
@@ -11,30 +11,29 @@ const News = () => {
 
       <div className="image-container">
         <div className="news1">
+          <img src="/public/assets/images/codGpass.jpeg" alt="" />
           <div className="new1-content">
             <h3>COD to go to Game Pass in gaming shake-up</h3>
-            <p>August 24</p>
+            <h5>August 24</h5>
             <p>
               Microsoft has torn up the rules of big video game launches by
               announcing its most eagerly awaited new title...
             </p>
           </div>
         </div>
-        <div className="news2">
-          <h2>
-            Fortnite released a Battle Pass item to shop, causing a huge
-            backlash among the fans
-          </h2>
-          <p>July 24</p>
-          <p>
-            Nobody likes their stuff being removed due to an error, even when
-            recompensed. Of course that stings, and Fortnite players had to
-            endure just that recently. To put context, there was an error in the
-            Item Shop today...
-          </p>
+        <div className="news1">
+          <img src="/public/assets/images/fortnitebpass.jpg" alt="" />
+          <div className="new1-content">
+            <h3>Battle Pass item causing a huge backlash among the fans</h3>
+            <h5>July 24</h5>
+            <p>
+              Nobody likes their stuff being removed due to an error, even when
+              recompensed. Of course that stings, and Fortnite players...
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
