c853fb1c0c5397dfc3f582b34f644d75bd1d873b
FaridYoucef <songortekinn@gmail.com>
2024-08-15 08:37:51 +0100

    -Style the responsive design -Add a div playe-info to allow the horizontal postion of the cards' elements -Remove the 90% max-width and replace it with 10px padding -Style the tiltle -Display the player cards as columns -Display the card player elements row -Remove the buttons when small screen

--- src/components/PlayersPrise.jsx
+++ src/components/PlayersPrise.jsx
@@ -3,7 +3,7 @@ import "./styles/playerPrise.css";
 
 function PlayersPrise() {
   return (
-    <div className="player-container">    
+    <div className="player-container">
       <div className="player-title">
         <h1>PLAYER OF THE WEEK</h1>
         <p>Test your skills and see your name every Sunday at 11AM</p>
@@ -12,24 +12,30 @@ function PlayersPrise() {
       <div className="palyerCard-container">
         <div className="palyer-card">
           <img src="/assets/images/player.jpg" alt="player" />
-          <h1>Elgorn</h1>
-          <p>1354 XP gained 08/26-09/02</p>
-          <p className="coins">+20 Xcoins</p>
-          <button>View Profile</button>
+          <div className="player-info">
+            <h1>Elgorn</h1>
+            <p>1354 XP gained 08/26-09/02</p>
+            <p className="coins">+20 Xcoins</p>
+            <button>View Profile</button>
+          </div>
         </div>
         <div className="palyer-card card-1">
           <img src="/assets/images/player1.jpg" alt="player" />
-          <h1>MasterofDesaster</h1>
-          <p>1354 XP gained 08/26-09/02</p>
-          <p className="coins">+20 Xcoins</p>
-          <button>View Profile</button>
+          <div className="player-info">
+            <h1>MasterofDesaster</h1>
+            <p>1354 XP gained 08/26-09/02</p>
+            <p className="coins">+20 Xcoins</p>
+            <button>View Profile</button>
+          </div>
         </div>
         <div className="palyer-card">
           <img src="/assets/images/player2.webp" alt="player" />
-          <h1>Setou1</h1>
-          <p>1354 XP gained 08/26-09/02</p>
-          <p className="coins">+20 Xcoins</p>
-          <button>View Profile</button>
+          <div className="player-info">
+            <h1>Setou1</h1>
+            <p>1354 XP gained 08/26-09/02</p>
+            <p className="coins">+20 Xcoins</p>
+            <button>View Profile</button>
+          </div>
         </div>
       </div>
     </div>
